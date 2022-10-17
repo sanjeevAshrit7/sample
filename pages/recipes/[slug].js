c42d449702043fd52534939f37334c228fa4aac8
sanjeevAshrit7 <sanjeev@zerozilla.com>
2022-10-17 15:27:03 +0530

    initial commit4

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -34,94 +34,94 @@ export default function OneRecipe({ data }) {
     const [likes, setLikes] = useState(data?.recipe?.likes);
     const router = useRouter();
 
-    if(router?.isFallback) {
-        return(
-            <div>
-                Loading ....
-            </div>
-        )
-    }
-
     const addLikes = async () => {
         const res = await fetch('/api/handle-likes', {
             method: 'POST',
-            body: JSON.stringify({_id: recipe?._id})
+            body: JSON.stringify({ _id: recipe?._id })
         }
         ).catch((error) => console.log(error))
 
         const data = await res.json();
 
-        setLikes(data?.likes);  
+        setLikes(data?.likes);
     }
     // console.log('recipe', recipe)
     // console.log('data', data)
-    return (
-        <article>
-            <h1>{recipe?.name}</h1>
-            <button
-                onClick={addLikes}
-                className={'like-button'}
-            >
-                {likes} ❤️
-            </button>
-            <main>
-                {/* <img src={urlFor(recipe?.mainImage).url()} /> */}
-                <div>
-                    <Card css={{ backgroundColor: '$accents4'}}>
-                        <Card.Header>
-                            <Card.Image
-                                src={urlFor(recipe?.mainImage)}
-                                objectFit='contain'
-                                width="100%"
-                                height={140}
-                                alt={'NA'}
-                            />
-                            <Grid.Container css={{ backgroundColor: '$accents1', px: '$10' }} >
-                                <Col>
-                                <h4>Ingridients for {recipe?.name}</h4>
-                                {recipe?.ingridient?.map((ingridient) => {
-                                    console.log('ingridient',ingridient)
-                                    return (
-                                        <Row css={{alignItems: 'center', justifyContent: 'flex-start', alignSelf: 'center'}} key={ingridient?._key}>
-                                            {/* <Image src={urlFor(ingridient?.ingrident?.image)} alt={'NA'} height={20} width={30}/> */}
-                                            <Text h4 css={{}}>
-                                            <Image src={urlFor(ingridient?.ingrident?.image)} alt={'NA'} height={20} width={30}/>
-                                            ({ingridient?.wholeNumber})
-                                            {ingridient?.fraction}
-                                            {" "}
-                                            {ingridient?.unit}
-                                            {""}
-                                            {ingridient?.ingrident?.name}
-                                            </Text>
-                                        </Row>
-                                    )
-                                })
-                                }
-                                </Col>
-                            </Grid.Container>
-                        </Card.Header>
-                        <Card.Body>
-                            <Card.Body css={{backgroundColor: '$primary', flexDirection: 'column', width: 'stretch', marginRight: '$10', borderRadius: '$base',}}>
-                                <Col justify="center" align="center">
-                                    <Text h6 size={15} color="white" css={{ m: 0, textAlign: 'justify' }}>
-                                        {/* NextUI gives you the best developer experience with all the features
+    if (router?.isFallback) {
+        return (
+            <div>
+                Loading ....
+            </div>
+        )
+    } else {
+        return (
+            <article>
+                <h1>{recipe?.name}</h1>
+                <button
+                    onClick={addLikes}
+                    className={'like-button'}
+                >
+                    {likes} ❤️
+                </button>
+                <main>
+                    {/* <img src={urlFor(recipe?.mainImage).url()} /> */}
+                    <div>
+                        <Card css={{ backgroundColor: '$accents4' }}>
+                            <Card.Header>
+                                <Card.Image
+                                    src={urlFor(recipe?.mainImage)}
+                                    objectFit='contain'
+                                    width="100%"
+                                    height={140}
+                                    alt={'NA'}
+                                />
+                                <Grid.Container css={{ backgroundColor: '$accents1', px: '$10' }} >
+                                    <Col>
+                                        <h4>Ingridients for {recipe?.name}</h4>
+                                        {recipe?.ingridient?.map((ingridient) => {
+                                            console.log('ingridient', ingridient)
+                                            return (
+                                                <Row css={{ alignItems: 'center', justifyContent: 'flex-start', alignSelf: 'center' }} key={ingridient?._key}>
+                                                    {/* <Image src={urlFor(ingridient?.ingrident?.image)} alt={'NA'} height={20} width={30}/> */}
+                                                    <Text h4 css={{}}>
+                                                        <Image src={urlFor(ingridient?.ingrident?.image)} alt={'NA'} height={20} width={30} />
+                                                        ({ingridient?.wholeNumber})
+                                                        {ingridient?.fraction}
+                                                        {" "}
+                                                        {ingridient?.unit}
+                                                        {""}
+                                                        {ingridient?.ingrident?.name}
+                                                    </Text>
+                                                </Row>
+                                            )
+                                        })
+                                        }
+                                    </Col>
+                                </Grid.Container>
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Body css={{ backgroundColor: '$primary', flexDirection: 'column', width: 'stretch', marginRight: '$10', borderRadius: '$base', }}>
+                                    <Col justify="center" align="center">
+                                        <Text h6 size={15} color="white" css={{ m: 0, textAlign: 'justify' }}>
+                                            {/* NextUI gives you the best developer experience with all the features
                                         you need for building beautiful and modern websites and
                                         applications. */}
-                                        <PortableText
-                                            value={recipe?.instructions}
-                                        />
-                                    </Text>
-                                </Col>
+                                            <PortableText
+                                                value={recipe?.instructions}
+                                            />
+                                        </Text>
+                                    </Col>
+                                </Card.Body>
                             </Card.Body>
-                    </Card.Body>
-                    </Card>
-                    {/* <PortableText
+                        </Card>
+                        {/* <PortableText
                         value={recipe?.instructions}
                     /> */}
-                </div>
-            </main>
-        </article>
-    )
+                    </div>
+                </main>
+            </article>
+        )
+    }
 }
 
 
@@ -147,4 +147,4 @@ export async function getStaticProps({ params }) {
             data: { recipe }
         }
     }
-}
+};
